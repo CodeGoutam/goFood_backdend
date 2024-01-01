f3f6e04a7ba056994f7a47358bb3f8fb2e475392
CodeGoutam <hgoutam2001@gmail.com>
2024-01-01 22:20:23 +0530

    change1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+// const cors = require('cors')
 app.use((req, res, next) => {
   
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
     );
     next();
 });
-app.use(cors())
+// app.use(cors())
 app.use(express.json());
 express.urlencoded({ extended: true });
 app.use("/api", require("./foodItems"));
