d25e93a0d4a1a1e74ce93b070db87a2cb4502eb4
CodeGoutam <hgoutam2001@gmail.com>
2024-01-01 22:51:00 +0530

    change4

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 // const cors = require('cors')
-app.use((req, res, next) => {
-  
-    res.setHeader("Access-Control-Allow-Origin", "https://6592f199541fd5ddaa005114--imaginative-mooncake-18f71d.netlify.app/");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
+// app.use((req, res, next) => {
+//   
+//     res.setHeader("Access-Control-Allow-Origin", "https://6592f199541fd5ddaa005114--imaginative-mooncake-18f71d.netlify.app/");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// });
 // app.use(cors())
 app.use(express.json());
 express.urlencoded({ extended: true });
