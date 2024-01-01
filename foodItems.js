39e821a29d167e95d5dddb6beef3b44e34a81690
CodeGoutam <hgoutam2001@gmail.com>
2024-01-01 21:59:43 +0530

    change

--- foodItems.js
+++ foodItems.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 let foodItems, foodCat;
 async function display() {
-    await mongoose.connect("mongodb://localhost:27017/gofood");
+    // await mongoose.connect("mongodb://localhost:27017/gofood");
+    await mongoose.connect("mongodb+srv://gofood:gofood@cluster0.qjptync.mongodb.net/?retryWrites=true&w=majority");
     console.log("db connectedd");
     const items = mongoose.connection.db.collection("food_items");
     foodItems = await items.find({}).toArray();
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use((req, res, next) => {
+    // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
         "Access-Control-Allow-Headers",
