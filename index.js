ea8ed152a643200366c52f2b748d2d99e6bb91bf
CodeGoutam <hgoutam2001@gmail.com>
2024-01-02 16:30:09 +0530

    change10

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 // const cors = require('cors')
+const FRONT_URL = process.env.FRONT_URL
 app.use((req, res, next) => {
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", `${FRONT_URL} || http://localhost:3000`);
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
+    console.log("FRONT END URL", FRONT_URL);
+    console.log("BACKEND URL", process.env.BASE_URL);
     next();
 });
 // app.use(cors())
@@ -18,7 +21,8 @@ app.use("/api", require("./signUp"));
 app.use("/api", require("./signIn"));
 app.use("/api", require("./Orders"));
 app.use("/api", require("./Myorders"));
-app.use("/", () => {
+app.use("/", (req, res) => {
+    res.send("I m backend ")
     console.log("backend")
 })
 
